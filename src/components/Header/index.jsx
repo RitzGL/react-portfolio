6da2b30da0c8a1f067d1a8d82f2b864515ed7c8f
Ritz GL <maurigarzonlara@hotmail.com>
2021-07-26 13:33:51 +0800

    Basic spacing
    Added basic positioning of certain header children

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./styles.css";
 // This function will return a header
 // Appears on multiple pages
 // contains links to the other pages
@@ -7,32 +7,30 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <nav>
-        <div className="title">
-          <div>
-            <h1>RitzGL</h1>
-            <h3>Web Developer</h3>
-          </div>
-          <ul>
-            <li>
-              <a href="/About"> About </a>
-            </li>
-            <li>
-              <a href="/projects"> Projects </a>
-            </li>
-            <li>
-              <a href="/contact"> Contact </a>
-            </li>
-            <li>
-              <a
-                href="../../../public/assets/Developer Resume.pdf"
-                download="Resume"
-              >
-                Resume
-              </a>
-            </li>
-          </ul>
+      <nav className='navigation'>
+        <div className='heading'>
+          <h1>RitzGL</h1>
+          <h3>Web Developer</h3>
         </div>
+        <ul>
+          <li>
+            <a href="/About"> About </a>
+          </li>
+          <li>
+            <a href="/projects"> Projects </a>
+          </li>
+          <li>
+            <a href="/contact"> Contact </a>
+          </li>
+          <li>
+            <a
+              href="../../../public/assets/Developer Resume.pdf"
+              download="Resume"
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
       </nav>
     </header>
   );
