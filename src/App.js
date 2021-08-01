79766e3dbe6bb1a5ebcb69cbdd87f4ce1ff050b4
Ritz GL <maurigarzonlara@hotmail.com>
2021-08-01 14:37:53 +0800

    Component renamed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Hero from './components/Hero'
+import Background from './components/Background'
 function App() {
   // This is where the components will be nested and conditionally rendered
   return (
     <>
       <Header />
-      <Hero />
+      <Background />
       <Footer />
     </>
   );
