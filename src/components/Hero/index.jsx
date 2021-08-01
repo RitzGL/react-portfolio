a0fb005b47be6bac862e93b6b32a515ebca5fac3
Ritz GL <maurigarzonlara@hotmail.com>
2021-08-01 14:36:21 +0800

    Header, Footer, Backgroun
    Navigation will be nested inside the hero

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+
+function Hero() {
+  return (
+    <div id="container">
+      <img src="hero-banner.jpg" alt="large graphic" className="bg" />
+    </div>
+  );
+}
+
+export default Hero;
